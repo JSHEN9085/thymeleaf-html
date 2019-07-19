2fcb70de00a96c46b2f388b2b20dbf038421caa6
JSHEN9085 <jshen9085@gmail.com>
2019-07-18 22:11:16 -0400

    employee controller

---
+++ src/main/java/com/jshen/thymeleaf/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package com.jshen.thymeleaf.controller;
+
+import com.jshen.thymeleaf.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    //load employees data
+    List<Employee> employees;
+
+    @PostConstruct
+    private void loadData(){
+        //create employee
+        Employee employee1 = new Employee(1, "Lesila", "Andrew", "lesila.A@gmail.com");
+
+        //create the list
+        employees = new ArrayList<>();
+
+        //add employee
+        employees.add(employee1);
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model){
+        
+    }
+}
--- src/main/java/com/jshen/thymeleaf/model/Employee.java
+++ src/main/java/com/jshen/thymeleaf/model/Employee.java
@@ -4,6 +4,58 @@ public class Employee {
     private int id;
     public String firstName;
     public String lastName;
-    public String email; 
+    public String email;
 
+    public Employee(){
+
+    }
+
+    public Employee(int id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
