cc00be6fa8e07ef53c93a9ac7bd66c714d01677f
JSHEN9085 <jshen9085@gmail.com>
2019-07-25 09:52:26 -0400

    connected to sql

--- src/main/java/com/jshen/thymeleaf/controller/EmployeeController.java
+++ src/main/java/com/jshen/thymeleaf/controller/EmployeeController.java
@@ -1,35 +1,28 @@
 package com.jshen.thymeleaf.controller;
 
 import com.jshen.thymeleaf.entity.Employee;
+import com.jshen.thymeleaf.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
-    //load employees data
-    List<Employee> employees;
 
-    @PostConstruct
-    private void loadData(){
-        //create employee
-        Employee employee1 = new Employee(1, "Lesila", "Andrew", "lesila.A@gmail.com");
+    private EmployeeService employeeService;
 
-        //create the list
-        employees = new ArrayList<>();
-
-        //add employee
-        employees.add(employee1);
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping("/list")
     public String listEmployees(Model model){
+        List<Employee> employees = employeeService.findAll();
+
         model.addAttribute("employees", employees);
         return "list-employees";
     }
